08d35b183fa30254bbe3b6bd7e585ab1a0a9049b
Dropio12 <anass.elhallaoui@gmail.com>
2022-10-16 00:47:14 -0400

    Upgrade from the ML hackathon

---
+++ ExtractTextFromImg.py
@@ -0,0 +1,33 @@
+# text recognition
+import cv2
+import pytesseract
+from PIL import Image
+import os
+
+def image_to_text(image):
+    path_to_tesseract = r"C:\Users\Strea\AppData\Local\Tesseract-OCR\tesseract.exe"
+    pytesseract.tesseract_cmd = path_to_tesseract
+    # read the image
+    img = cv2.imread(image)
+    # convert to gray
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # apply threshold
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # save the filtered image in the /tmp directory
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, thresh)
+    # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
+    text = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    return text
+
+image_to_text('quotes.jpg')
+## read image
+#img = cv2.imread('quotes.jpg')
+
+# pytessercat
+#pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+#text = pytesseract.image_to_string
+
+# print text
+#print(text)
---
+++ main.py
@@ -0,0 +1,5 @@
+import MLrecognition
+from ExtractTextFromImg import image_to_text
+
+
+MLrecognition(image_to_text('quotes.jpg'))
