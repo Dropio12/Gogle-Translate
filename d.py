e6e6ae6040f8ee58a6e9dc21d4c7fbbec20c54d4
Dropio12 <anass.elhallaoui@gmail.com>
2022-11-17 22:43:26 -0500

    Save

--- d.py
+++ d.py
@@ -2,6 +2,40 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 Builder.load_string('''
+ScreenManager:
+    
+<login>:
+    orientation: 'vertical'
+    padding: 10
+    spacing: 10
+    MDLabel:
+        text: 'Login'
+        halign: 'center'
+        font_style: 'H4'
+        theme_text_color: 'Custom'
+        text_color: 0, 0, 0, 1
+    MDTextField:
+        id: username
+        hint_text: 'Username'
+        helper_text: 'or email'
+        helper_text_mode: 'on_focus'
+        icon_right: 'account'
+        icon_right_color: app.theme_cls.primary_color
+        required: True
+    MDTextField:
+        id: password
+        hint_text: 'Password'
+        helper_text: 'Forgot password?'
+        helper_text_mode: 'on_focus'
+        icon_right: 'eye-off'
+        icon_right_color: app.theme_cls.primary_color
+        required: True
+        password: True
+    MDRaisedButton:
+        text: 'Login'
+        pos_hint: {'center_x': .5}
+        on_release: app.login()
+        
 <CameraClick>:
     orientation: 'vertical'
     Camera:
